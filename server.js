954738aee2cc1e082d7090ae555548369eb0ab99
Glitch (glitch-blank-node) <none>
2024-02-20 19:13:43 +0000

    🏀🚧 Checkpoint ./server.js:65222473/1556

--- server.js
+++ server.js
@@ -1,56 +1,11 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const hbs = require('hbs')
-const collection=require('./mongodb')
-const bcrypt = require('bcrypt')
-const templatePath=path.join(__dirname,'templates')
-const publicPath=path.join(__dirname, 'public')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(publicPath))
-app.set("view engine","hbs")
-app.set("views",templatePath)
-app.use(express.urlencoded({extended:false}))
+app.get('/', (req, res) => {
+  res.send('Ahoj Adame, ty magore ono to jede, JUPI!');
+});
 
-app.get("/",(req, res)=>{
-    res.render("login");
-})
-
-app.get("/signup",(req, res)=>{
-    res.render("signup");
-})
-
-app.post("/signup",async (req,res)=>{
-
-    const data={
-        name:req.body.name,
-        password: await bcrypt.hash(req.body.password, 10)
-    }
-
-    await collection.insertMany([data])
-
-    res.render("home")
-
-})
-
-app.post("/login",async (req,res)=>{
-
-    try{
-        const user=await collection.findOne({name:req.body.name})
-
-        if(user && await bcrypt.compare(req.body.password, user.password)){
-            res.render("home")
-        }
-        else{
-            res.status(400).render("login",{ errorMessage: "Špatné heslo" })
-        }
-    }
-    catch{
-        res.status(400).render("login", { errorMessage: "Špatné údaje" })
-    }
-
-})
-
-app.listen(3000,() => {
-    console.log('WEB IS LIVE!');
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
