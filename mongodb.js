f2f29fbcf8cb47958cf99c155c50285003ef752c
Glitch (glitch-blank-node) <none>
2024-02-21 17:39:04 +0000

    🏇🚆 Checkpoint ./mongodb.js:65222473/1180 ./public/home.css:65222473/131

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-// Update the connection string with your MongoDB Atlas credentials and database name
-const atlasConnectionString = "mongodb+srv://simvesely:xaghoc-fyscop-1neNki@cluster0.wg2ime2.mongodb.net/helpdesk?retryWrites=true&w=majority";
+const atlasConnectionString = "mongodb+srv://simvesely:Mongo123@cluster0.wg2ime2.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(atlasConnectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  //useUnifiedTopology: true
 })
   .then(() => {
     console.log("MongoDB connected");
@@ -25,34 +24,6 @@ const LogInSchema = new mongoose.Schema({
   }
 });
 
-// Update the model name to match your collection in MongoDB Atlas
 const collection = mongoose.model("collection1", LogInSchema);
 
 module.exports = collection;
-
-/*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://simvesely:xaghoc-fyscop-1neNki@cluster0.wg2ime2.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);*/
